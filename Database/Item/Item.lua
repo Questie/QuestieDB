a8c3d05587853f0403766066032b7468a0ff2e85
Logon <Logon.q@gmail.com>
2023-07-25 22:12:02 +0200

    Clean up of structure comments

--- Database/Item/Item.lua
+++ Database/Item/Item.lua
@@ -19,24 +19,6 @@ do
   local getNumber = Database.getNumber
   local getTable = Database.getTable
 
-  -- itemKeysOriginal = {
-  --   ['name'] = 1,           -- string
-  --   ['npcDrops'] = 2,       -- table or nil, NPC IDs
-  --   ['objectDrops'] = 3,    -- table or nil, object IDs
-  --   ['itemDrops'] = 4,      -- table or nil, item IDs
-  --   ['startQuest'] = 5,     -- int or nil, ID of the quest started by this item
-  --   ['questRewards'] = 6,   -- table or nil, quest IDs
-  --   ['flags'] = 7,          -- int or nil, see: https://github.com/cmangos/issues/wiki/Item_template#flags
-  --   ['foodType'] = 8,       -- int or nil, see https://github.com/cmangos/issues/wiki/Item_template#foodtype
-  --   ['itemLevel'] = 9,      -- int, the level of this item
-  --   ['requiredLevel'] = 10, -- int, the level required to equip/use this item
-  --   ['ammoType'] = 11,      -- int,
-  --   ['class'] = 12,         -- int,
-  --   ['subClass'] = 13,      -- int,
-  --   ['vendors'] = 14,       -- table or nil, NPC IDs
-  --   ['relatedQuests'] = 15, -- table or nil, IDs of quests that are related to this item
-  -- }
-
   -- 1. ['name']           -- string
   -- 2. ['npcDrops']       -- table or nil, NPC IDs
   -- 3. ['objectDrops']    -- table or nil, object IDs
