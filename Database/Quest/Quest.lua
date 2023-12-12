19189dadb6b02233a9243974c37a0660e090c84f
Logon <Logon.q@gmail.com>
2023-12-12 15:02:02 +0100

    It should be reputationReward apperently.

--- Database/Quest/Quest.lua
+++ Database/Quest/Quest.lua
@@ -84,49 +84,6 @@ do
   --   --['itemObjective'] = 3, -- table {{item(int), text(string)},...}
   --   --['reputationObjective'] = 4, -- table: {faction(int), value(int)}
   --   --['killCreditObjective'] = 5, -- table: {{{creature(int), ...}, baseCreatureID, baseCreatureText}, ...}
-  --   ['sourceItemId'] = 11,           -- int, item provided by quest starter
-  --   ['preQuestGroup'] = 12,          -- table: {quest(int)}
-  --   ['preQuestSingle'] = 13,         -- table: {quest(int)}
-  --   ['childQuests'] = 14,            -- table: {quest(int)}
-  --   ['inGroupWith'] = 15,            -- table: {quest(int)}
-  --   ['exclusiveTo'] = 16,            -- table: {quest(int)}
-  --   ['zoneOrSort'] = 17,             -- int, >0: AreaTable.dbc ID; <0: QuestSort.dbc ID
-  --   ['requiredSkill'] = 18,          -- table: {skill(int), value(int)}
-  --   ['requiredMinRep'] = 19,         -- table: {faction(int), value(int)}
-  --   ['requiredMaxRep'] = 20,         -- table: {faction(int), value(int)}
-  --   ['requiredSourceItems'] = 21,    -- table: {item(int), ...} Items that are not an objective but still needed for the quest.
-  --   ['nextQuestInChain'] = 22,       -- int: if this quest is active/finished, the current quest is not available anymore
-  --   ['questFlags'] = 23,             -- bitmask: see https://github.com/cmangos/issues/wiki/Quest_template#questflags
-  --   ['specialFlags'] = 24,           -- bitmask: 1 = Repeatable, 2 = Needs event, 4 = Monthly reset (req. 1). See https://github.com/cmangos/issues/wiki/Quest_template#specialflags
-  --   ['parentQuest'] = 25,            -- int, the ID of the parent quest that needs to be active for the current one to be available. See also 'childQuests' (field 14)
-  --   ['rewardReputation'] = 26,       --table: {{faction(int), value(int)},...}, a list of reputation rewarded upon quest completion
-  --   ['extraObjectives'] = 27,        -- table: {{spawnlist, iconFile, text, objectiveIndex (optional), {{dbReferenceType, id}, ...} (optional)},...}, a list of hidden special objectives for a quest. Similar to requiredSourceItems
-  --   ['requiredSpell'] = 28,          -- int: quest is only available if character has this spellID
-  --   ['requiredSpecialization'] = 29, -- int: quest is only available if character meets the spec requirements. Use QuestieProfessions.specializationKeys for having a spec, or QuestieProfessions.professionKeys to indicate having the profession with no spec. See QuestieProfessions.lua for more info.
-  -- }
-
-  --? New
-  -- QuestieDB.questKeys = {
-  --   ['name'] = 1,      -- string
-  --   ['startedBy'] = 2, -- table
-  --   --['creatureStart'] = 1, -- table {creature(int),...}
-  --   --['objectStart'] = 2, -- table {object(int),...}
-  --   --['itemStart'] = 3, -- table {item(int),...}
-  --   ['finishedBy'] = 3, -- table
-  --   --['creatureEnd'] = 1, -- table {creature(int),...}
-  --   --['objectEnd'] = 2, -- table {object(int),...}
-  --   ['requiredLevel'] = 4,   -- int
-  --   ['questLevel'] = 5,      -- int
-  --   ['requiredRaces'] = 6,   -- bitmask
-  --   ['requiredClasses'] = 7, -- bitmask
-  --   ['objectivesText'] = 8,  -- table: {string,...}, Description of the quest. Auto-complete if nil.
-  --   ['triggerEnd'] = 9,      -- table: {text, {[zoneID] = {coordPair,...},...}}
-  --   ['objectives'] = 10,     -- table
-  --   --['creatureObjective'] = 1, -- table {{creature(int), text(string)},...}, If text is nil the default "<Name> slain x/y" is used
-  --   --['objectObjective'] = 2, -- table {{object(int), text(string)},...}
-  --   --['itemObjective'] = 3, -- table {{item(int), text(string)},...}
-  --   --['reputationObjective'] = 4, -- table: {faction(int), value(int)}
-  --   --['killCreditObjective'] = 5, -- table: {{creature(int), ...}, baseCreatureID, baseCreatureText}
   --   --['spellObjective'] = 6, -- table: {{spell(int), text(string)},...}
   --   ['sourceItemId'] = 11,           -- int, item provided by quest starter
   --   ['preQuestGroup'] = 12,          -- table: {quest(int)}
@@ -143,7 +100,7 @@ do
   --   ['questFlags'] = 23,             -- bitmask: see https://github.com/cmangos/issues/wiki/Quest_template#questflags
   --   ['specialFlags'] = 24,           -- bitmask: 1 = Repeatable, 2 = Needs event, 4 = Monthly reset (req. 1). See https://github.com/cmangos/issues/wiki/Quest_template#specialflags
   --   ['parentQuest'] = 25,            -- int, the ID of the parent quest that needs to be active for the current one to be available. See also 'childQuests' (field 14)
-  --   ['reputationReward'] = 26,       -- table: {{FACTION,VALUE}, ...}, A list of reputation reward for factions
+  --   ['reputationReward'] = 26,       --table: {{faction(int), value(int)},...}, a list of reputation rewarded upon quest completion
   --   ['extraObjectives'] = 27,        -- table: {{spawnlist, iconFile, text, objectiveIndex (optional), {{dbReferenceType, id}, ...} (optional)},...}, a list of hidden special objectives for a quest. Similar to requiredSourceItems
   --   ['requiredSpell'] = 28,          -- int: quest is only available if character has this spellID
   --   ['requiredSpecialization'] = 29, -- int: quest is only available if character meets the spec requirements. Use QuestieProfessions.specializationKeys for having a spec, or QuestieProfessions.professionKeys to indicate having the profession with no spec. See QuestieProfessions.lua for more info.
@@ -529,9 +486,9 @@ do
   ---Returns the reward reputation of the quest.
   ---@param id QuestId
   ---@return ReputationPair[]?
-  function Quest.rewardReputation(id)
+  function Quest.reputationReward(id)
     if override[id] then
-      return override[id]["rewardReputation"]
+      return override[id]["reputationReward"]
     end
     local data = glob[id]
     if data then
