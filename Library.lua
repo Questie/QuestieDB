188189db23e21def24d812092c4042095463104b
Logon <Logon.q@gmail.com>
2023-12-13 23:22:47 +0100

    Restructured the outward facing code a bit more.

--- Library.lua
+++ Library.lua
@@ -1,3 +1,44 @@
+------------------------------------------------------
+--   ____                  _   _      _____  ____
+--  / __ \                | | (_)    |  __ \|  _ \
+-- | |  | |_   _  ___  ___| |_ _  ___| |  | | |_) |
+-- | |  | | | | |/ _ \/ __| __| |/ _ \ |  | |  _ <
+-- | |__| | |_| |  __/\__ \ |_| |  __/ |__| | |_) |
+--  \___\_\\__,_|\___||___/\__|_|\___|_____/|____/
+------------------------------------------------------
+--* This file is the main file for the QuestieDB library.
+--* It contains the main functions for the library.
+--
+-- The library is split into two namespaces, Public and Private.
+--* The Public namespace is the one that is returned when you call LibQuestieDB()
+-- The Private namespace is the one that is used internally in the library.
+
+---@class QuestieDB
+---@field public Quest QuestFunctions
+---@field public Item ItemFunctions
+---@field public Npc NpcFunctions
+---@field public Object ObjectFunctions
+local PublicLibQuestieDB = {
+  Quest = {},
+  Item = {},
+  Npc = {},
+  Object = {},
+}
+
+---Get LibQuestieDB
+---@return QuestieDB
+function LibQuestieDB()
+  ---@type QuestieDB
+  return setmetatable(PublicLibQuestieDB, {
+    __newindex = function(_, _, _)
+      error("Attempt to write to a read-only table")
+    end
+  })
+end
+
+
+---- Private namespace -----
+
 -- The main namespace class for the QuestieDB library.
 ---@class LibQuestieDB
 local PrivateLibQuestieDB = select(2, ...)
@@ -22,29 +63,9 @@ end
 PrivateLibQuestieDB:initNamespace()
 PrivateLibQuestieDB.initNamespace = nil -- Remove it, no one should be able to change it
 
----@class QuestieDB
----@field public Quest QuestFunctions
----@field public Item ItemFunctions
----@field public Npc NpcFunctions
----@field public Object ObjectFunctions
-local PublicLibQuestieDB = {
-  Quest = {},
-  Item = {},
-  Npc = {},
-  Object = {},
-}
+
 PrivateLibQuestieDB.PublicLibQuestieDB = PublicLibQuestieDB
 
----Get LibQuestieDB
----@return QuestieDB
-function LibQuestieDB()
-  ---@type QuestieDB
-  return setmetatable(PublicLibQuestieDB, {
-    __newindex = function(_, _, _)
-      error("Attempt to write to a read-only table")
-    end
-  })
-end
 
 do
   local savedFunction = LibQuestieDB
