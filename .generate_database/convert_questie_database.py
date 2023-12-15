7bd8edba6b43afcbde29a95742317861d66f570f
Logon <Logon.q@gmail.com>
2023-12-15 16:34:08 +0100

    Added some readme

--- .generate_database/convert_questie_database.py
+++ .generate_database/convert_questie_database.py
@@ -145,7 +145,28 @@ def applyCorrections(entity_data, entity_corrections):
 # These are all the same for all expansions
 replaceLookup = {
  "Item": {
-    "totalEndLength": 10, #The length after combining the data
+    # Example Item has 16 fields
+    # ['name'] = 1,             -- string
+    # ['npcDrops'] = 2,         -- table or nil, NPC IDs
+    # ['objectDrops'] = 3,      -- table or nil, object IDs
+    # ['itemDrops'] = 4,        -- table or nil, item IDs
+    # ['startQuest'] = 5,       -- int or nil, ID of the quest started by this item
+    # ['questRewards'] = 6,     -- table or nil, quest IDs
+    # ['flags'] = 7,            -- int or nil, see: https://github.com/cmangos/issues/wiki/Item_template#flags
+    # ['foodType'] = 8,         -- int or nil, see https://github.com/cmangos/issues/wiki/Item_template#foodtype
+    # ['itemLevel'] = 9,        -- int, the level of this item
+    # ['requiredLevel'] = 10,   -- int, the level required to equip/use this item
+    # ['ammoType'] = 11,        -- int,
+    # ['class'] = 12,           -- int,
+    # ['subClass'] = 13,        -- int,
+    # ['vendors'] = 14,         -- table or nil, NPC IDs
+    # ['relatedQuests'] = 15,   -- table or nil, IDs of quests that are related to this item
+    # ['teachesSpell'] = 16,    -- int, spellID taught by this item upon use
+    #
+    # We combine 7 fields into 1
+    # So 16 - 7 + 1 = 10
+    # 16 total, 7 combined into 1 = 10
+    "totalEndLength": 10, #The length of the array after combining the data
     "luaReplaceIndex": 7,
     "luaCombineIndex": [
       7,  #flags
@@ -158,7 +179,7 @@ replaceLookup = {
     ],
   },
   "Npc": {
-    "totalEndLength": 7, #The length after combining the data
+    "totalEndLength": 7, #The length of the array after combining the data
     "luaReplaceIndex": 2,
     "luaCombineIndex": [
       2, # minLevelHealth
@@ -173,12 +194,12 @@ replaceLookup = {
     ],
   },
   "Object": {
-    "totalEndLength": 6, #The length after combining the data
+    "totalEndLength": 6, #The length of the array after combining the data
     "luaReplaceIndex": None,
     "luaCombineIndex": None,
   },
   "Quest": {
-    "totalEndLength": 30, #The length after combining the data
+    "totalEndLength": 30, #The length of the array after combining the data
     "luaReplaceIndex": None,
     "luaCombineIndex": None,
   },
