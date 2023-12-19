10c73be96145204534ac2e054128e23f3b59636f
David Holmstedt <david.holmstedt@cambio.se>
2023-12-19 15:43:51 +0100

    Download the required files using wget

--- .generate_database/dumpDB.sh
+++ .generate_database/dumpDB.sh
@@ -2,10 +2,30 @@
 
 cd QuestieDB
 
-# Make _data folders
+# Create directories if they don't exist
 mkdir -p ./.generate_database/_data/Era
 mkdir -p ./.generate_database/_data/Tbc
 mkdir -p ./.generate_database/_data/Wotlk
+
+# Download files for Era
+wget -nv -O ./.generate_database/_data/Era/classicItemDB.lua https://raw.githubusercontent.com/Questie/Questie/master/Database/Classic/classicItemDB.lua &
+wget -nv -O ./.generate_database/_data/Era/classicNpcDB.lua https://raw.githubusercontent.com/Questie/Questie/master/Database/Classic/classicNpcDB.lua &
+wget -nv -O ./.generate_database/_data/Era/classicObjectDB.lua https://raw.githubusercontent.com/Questie/Questie/master/Database/Classic/classicObjectDB.lua &
+wget -nv -O ./.generate_database/_data/Era/classicQuestDB.lua https://raw.githubusercontent.com/Questie/Questie/master/Database/Classic/classicQuestDB.lua &
+
+# Download files for Tbc
+wget -nv -O ./.generate_database/_data/Tbc/tbcItemDB.lua https://raw.githubusercontent.com/Questie/Questie/master/Database/TBC/tbcItemDB.lua &
+wget -nv -O ./.generate_database/_data/Tbc/tbcNpcDB.lua https://raw.githubusercontent.com/Questie/Questie/master/Database/TBC/tbcNpcDB.lua &
+wget -nv -O ./.generate_database/_data/Tbc/tbcObjectDB.lua https://raw.githubusercontent.com/Questie/Questie/master/Database/TBC/tbcObjectDB.lua &
+wget -nv -O ./.generate_database/_data/Tbc/tbcQuestDB.lua https://raw.githubusercontent.com/Questie/Questie/master/Database/TBC/tbcQuestDB.lua &
+
+# Download files for Wotlk
+wget -nv -O ./.generate_database/_data/Wotlk/wotlkItemDB.lua https://raw.githubusercontent.com/Questie/Questie/master/Database/Wotlk/wotlkItemDB.lua &
+wget -nv -O ./.generate_database/_data/Wotlk/wotlkNpcDB.lua https://raw.githubusercontent.com/Questie/Questie/master/Database/Wotlk/wotlkNpcDB.lua &
+wget -nv -O ./.generate_database/_data/Wotlk/wotlkObjectDB.lua https://raw.githubusercontent.com/Questie/Questie/master/Database/Wotlk/wotlkObjectDB.lua &
+wget -nv -O ./.generate_database/_data/Wotlk/wotlkQuestDB.lua https://raw.githubusercontent.com/Questie/Questie/master/Database/Wotlk/wotlkQuestDB.lua &
+
+wait
 
 # Each expansion has its own dump process for each type
 lua ./.generate_database/createStatic.lua
