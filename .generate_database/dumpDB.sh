6871c16019933824535d7f2cb632b7be66a52ff3
David Holmstedt <david.holmstedt@cambio.se>
2023-12-19 15:55:19 +0100

    Replaced the dumping using python with internal dumping from the Lua.

--- .generate_database/dumpDB.sh
+++ .generate_database/dumpDB.sh
@@ -30,27 +30,17 @@ wait
 # Each expansion has its own dump process for each type
 lua ./.generate_database/createStatic.lua
 
-echo "Done dumping corrections - createStatic.lua"
+echo "Done dumping database - createStatic.lua"
 
 # Change to the generate_database directory
 cd .generate_database
 
-echo "Dumping database files"
-
-# Generate the full database files with corrections
-python3 ./convert_questie_database.py Era &
-python3 ./convert_questie_database.py Tbc &
-python3 ./convert_questie_database.py Wotlk &
-
-wait
-
+# Generate the HTML files
+# python3 ./dump.py Era &
+# python3 ./dump.py Tbc &
+# python3 ./dump.py Wotlk &
 echo "Done dumping HTML files"
 
-# Generate the HTML files
-python3 ./dump.py Era &
-python3 ./dump.py Tbc &
-python3 ./dump.py Wotlk &
-
 wait
 
-echo "Done"
+echo "dumpDB.sh Finished."
