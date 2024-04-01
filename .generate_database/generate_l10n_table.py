511c44f42ac23aca5aeadcee88028e4fe0634e3d
Logon <Logon.q@gmail.com>
2024-04-01 21:20:42 +0200

    Initial commit for the l10n code.

---
+++ .generate_database/generate_l10n_table.py
@@ -0,0 +1,170 @@
+# A script that reads classic_locales.json and generates a table of locale
+# information for use in the localization system.
+import json
+from dataclasses import dataclass
+import os
+import sys
+import re
+from helpers import get_data_dir_path
+
+
+# Order Item, Npc, Object, Quest
+# "enUS": "", # English (US) # Yes EN is empty
+# "ptBR": "pt", # Portuguese (Brazil)
+# "ruRU": "ru", # Russian (Russia)
+# "deDE": "de", # German (Germany)
+# "koKR": "ko", # Korean (Korea)
+# "esES": "es", # Spanish (Spain)
+# "frFR": "fr", # French (France)
+# "zhCN": "cn",
+# The very special character https://www.compart.com/en/unicode/U+2016
+# Double Dagger
+# "‡"
+
+if __name__ == "__main__":
+  # if len(sys.argv) != 2:
+  #   print("Usage: python generate_l10n_table.py <path to classic_locales.json>")
+  #   sys.exit(1)
+
+  fileoutput = get_data_dir_path("l10n", "era") + "/l10nData.lua-table"
+
+  supportedLocales = ["enUS", "ptBR", "ruRU", "deDE", "koKR", "esES", "frFR", "zhCN"]
+  supportedDataTypes = ["item", "npc", "object", "quest"]
+
+  with open("./classic_locales.json", "r", encoding="utf-8") as f:
+    locales = json.load(f)
+  print(len(locales))
+
+  LocaleData = {}
+  for datatype, data in locales.items():
+    if datatype not in supportedDataTypes:
+      continue
+    for typeId, typeData in data.items():
+      if int(typeId) not in LocaleData:
+        LocaleData[int(typeId)] = {}
+      if datatype not in LocaleData[int(typeId)]:
+        # LocaleData[int(typeId)][datatype] = {}
+        LocaleData[int(typeId)][datatype] = []
+      for locale in supportedLocales:
+        # LocaleData[int(typeId)][datatype][locale] = typeData[locale]["name"]
+        if datatype == "item" or datatype == "object":
+          LocaleData[int(typeId)][datatype].append(typeData[locale]["name"])
+        if datatype == "npc":
+          subname = ""
+          # regex: <\/b><\/td><\/tr>\\n<tr><td>(.*?)<\/td><\/tr><tr>
+          # Extract the subname from the tooltip
+          if "tooltip" in typeData[locale]:
+            tooltip = typeData[locale]["tooltip"]
+            match = re.search(r"<\/b><\/td><\/tr>\\n<tr><td>(.*?)<\/td><\/tr><tr>", tooltip)
+            if match:
+              subname = match.group(1)
+
+          LocaleData[int(typeId)][datatype].append((typeData[locale]["name"], subname))
+        if datatype == "quest":
+          Description = ""
+          if "Description" in typeData[locale]:
+            Description = typeData[locale]["Description"]
+          Text = ""
+          if "Text" in typeData[locale]:
+            Text = typeData[locale]["Text"]
+          LocaleData[int(typeId)][datatype].append((typeData[locale]["Title"], Description, Text))
+
+    print(f"const {datatype}")
+
+  print("Deleting empty entries")
+  # Delete all empty entries
+  for dataid in list(LocaleData.keys()):
+    if len(LocaleData[dataid]) == 0:
+      del LocaleData[dataid]
+
+  print("Escaping ' in strings")
+  # Escape ' in strings
+  for dataid in LocaleData.keys():
+    for datatype in LocaleData[dataid].keys():
+      if datatype == "item" or datatype == "object":
+        for i in range(len(LocaleData[dataid][datatype])):
+          LocaleData[dataid][datatype][i] = LocaleData[dataid][datatype][i].replace("'", "\\'")
+      if datatype == "npc":
+        for i in range(len(LocaleData[dataid][datatype])):
+          LocaleData[dataid][datatype][i] = (LocaleData[dataid][datatype][i][0].replace("'", "\\'"), LocaleData[dataid][datatype][i][1].replace("'", "\\'"))
+      if datatype == "quest":
+        for i in range(len(LocaleData[dataid][datatype])):
+          LocaleData[dataid][datatype][i] = (
+            LocaleData[dataid][datatype][i][0].replace("'", "\\'"),
+            LocaleData[dataid][datatype][i][1].replace("'", "\\'"),
+            LocaleData[dataid][datatype][i][2].replace("'", "\\'"),
+          )
+
+  # print(LocaleData)
+  print("Writing to file", fileoutput)
+  with open(fileoutput, "w", encoding="utf-8") as f:
+    indentation = "  "
+    f.write("{\n")
+    for dataid in sorted(LocaleData.keys()):
+      data = LocaleData[dataid]
+
+      #! Item
+      f.write(f"[{dataid}] = " + "{\n")  # {json.dumps(data, ensure_ascii=False)}\n")
+      if "item" in data:
+        f.write(indentation + "-- Item\n")
+        f.write(indentation + "'" + "‡".join(data["item"]) + "',\n")
+      elif "npc" in data or "object" in data or "quest" in data:
+        f.write("nil,\n")
+
+      #! NPC
+      # { "name", "subname" }
+      if "npc" in data:
+        AllNames = []
+        AllSubnames = []
+        for npc in data["npc"]:
+          AllNames.append(npc[0])
+          AllSubnames.append(npc[1])
+        f.write(indentation + "{ -- Npc\n")
+        f.write((indentation * 2) + "'" + "‡".join(AllNames) + "',\n")
+        if "‡".join(AllSubnames) == "‡‡‡‡‡‡‡":
+          f.write((indentation * 2) + "nil,\n")
+        else:
+          f.write("    '" + "‡".join(AllSubnames) + "',\n")
+        f.write(indentation + "},\n")
+      elif "object" in data or "quest" in data:
+        f.write("nil,\n")
+
+      #! Object
+      if "object" in data:
+        f.write(indentation + "-- Object\n")
+        f.write(indentation + "'" + "‡".join(data["object"]) + "',\n")
+      elif "quest" in data:
+        f.write("nil,\n")
+
+      #! Quest
+      # { "Title", {"Description"}, {"Text"} }
+      if "quest" in data:
+        AllTitles = []
+        AllDescriptions = []
+        AllTexts = []
+        for quest in data["quest"]:
+          AllTitles.append(quest[0])
+          AllDescriptions.append(quest[1])
+          AllTexts.append(quest[2])
+        f.write(indentation + "{ -- Quest\n")
+
+        # ? Title
+        f.write((indentation * 2) + "'" + "‡".join(AllTitles) + "',\n")
+
+        # ? Progress/Description
+        if "‡".join(AllDescriptions) == "‡‡‡‡‡‡‡":
+          f.write((indentation * 2) + "nil,\n")
+        else:
+          f.write((indentation * 2) + "'" + "‡".join(AllDescriptions) + "',\n")
+
+        # ? Quest Text
+        if "‡".join(AllTexts) == "‡‡‡‡‡‡‡":
+          f.write((indentation * 2) + "nil,\n")
+        else:
+          f.write((indentation * 2) + "'" + "‡".join(AllTexts) + "',\n")
+        f.write(indentation + "},\n")
+      # We don't need to write nil if it's the last entry
+
+      f.write("},\n")
+
+    f.write("}\n")
---
+++ Database/l10n/l10n.lua
@@ -0,0 +1,195 @@
+---@class LibQuestieDB
+---@field l10n l10n
+local LibQuestieDB = select(2, ...)
+
+local f = string.format
+
+---@class l10n:DatabaseType
+local l10n = LibQuestieDB.CreateDatabaseInTable(LibQuestieDB.l10n, "l10n", {})
+l10n.currentLocale = GetLocale()
+-- Set this to nil to use the locale of the client
+-- Override locale
+l10n.currentLocale = "ptBR"
+
+GLOBl10n = l10n
+
+-- Order Item, Npc, Object, Quest
+-- "enUS": "", # English (US) # Yes EN is empty
+-- "ptBR": "pt", # Portuguese (Brazil)
+-- "ruRU": "ru", # Russian (Russia)
+-- "deDE": "de", # German (Germany)
+-- "koKR": "ko", # Korean (Korea)
+-- "esES": "es", # Spanish (Spain)
+-- "frFR": "fr", # French (France)
+-- "zhCN": "cn",
+-- The very special character https://www.compart.com/en/unicode/U+2016
+-- Double Dagger
+-- "‡"
+-- Example:
+-- 'Worn Shortsword‡Espadim Usado‡Иссеченный короткий меч‡Abgenutztes Kurzschwert‡낡은 쇼트소드‡Espada corta desgastada‡Vieille épée courte‡破损的短剑'
+
+local specialChar = "‡"
+
+local indexToLocale = {
+  [1] = "enUS",
+  [2] = "ptBR",
+  [3] = "ruRU",
+  [4] = "deDE",
+  [5] = "koKR",
+  [6] = "esES",
+  [7] = "frFR",
+  [8] = "zhCN",
+}
+local localeToIndex = {}
+local localeToPattern = {}
+do
+  for k, v in pairs(indexToLocale) do
+    localeToIndex[v] = k
+  end
+
+  local function createPatternForLocale(locale)
+    local patternString = "^"
+    local repeatPattern = ".-"
+    local capturePattern = "(.-)"
+    for i = 1, localeToIndex[locale] do
+      patternString = patternString .. (i == localeToIndex[locale] and capturePattern or repeatPattern) .. (i == #indexToLocale and "$" or specialChar)
+    end
+    return patternString
+  end
+
+  for i = 1, #indexToLocale do
+    localeToPattern[indexToLocale[i]] = createPatternForLocale(indexToLocale[i])
+  end
+end
+
+-- /dump GLOBl10n.item(25)
+C_Timer.After(5, function()
+  print(localeToPattern[l10n.currentLocale])
+  local loc = l10n.AddStringGetter(1, "item")
+  local item = function(id, locale)
+    return loc(id):match(localeToPattern[locale])
+  end
+
+  DevTools_Dump(localeToPattern)
+  for locale in pairs(localeToPattern) do
+    print(locale, localeToPattern[locale])
+    print(item(25, locale))
+  end
+end)
+
+
+
+local function SetGetters()
+  -- ? Item
+  do
+    l10n.item = l10n.AddPatternGetter(1, "item", localeToPattern[l10n.currentLocale])
+
+    -- Returns item name i.e "Worn Shortsword"
+    l10n.itemName = l10n.item
+  end
+
+  -- ? Npc
+  do
+    local npcCache = {}
+    -- Gets the raw data for the npc
+    local npc = l10n.AddTableGetter(2, "npc")
+    l10n.npc = function(id)
+      if npcCache[id] then
+        return npcCache[id]
+      end
+
+      local data = npc(id)
+      if data then
+        --- This code gets the locale data for a single locale
+        --- The data objects contain all the locales in a single strings
+        --- Duplicated for performance reasons
+        local result = {}
+        for i = 1, #data do
+          local match = data[i]:match(localeToPattern[l10n.currentLocale])
+          result[i] = match ~= "" and match or nil
+        end
+        npcCache[id] = result
+        return result
+      end
+      return nil
+    end
+    -- Returns npc name i.e "Bran Bronzebeard"
+    l10n.npcName = function(id)
+      local npcData = npcCache[id] and npcCache[id] or l10n.npc(id)
+      return npcData and npcData[1] or nil
+    end
+    -- Returns npc subname i.e "Weapon Vendor"
+    l10n.npcSubName = function(id)
+      local npcData = npcCache[id] and npcCache[id] or l10n.npc(id)
+      return npcData and npcData[2] or nil
+    end
+  end
+
+  -- ? Object
+  do
+    l10n.object = l10n.AddPatternGetter(3, "object", localeToPattern[l10n.currentLocale])
+    -- Returns object name i.e "Worn Chest"
+    l10n.objectName = l10n.object
+  end
+
+  -- ? Quest
+  do
+    local questCache = {}
+    -- Gets the raw data for the quest
+    local quest = l10n.AddTableGetter(4, "quest")
+    l10n.quest = function(id)
+      if questCache[id] then
+        return questCache[id]
+      end
+
+      local data = quest(id)
+      if data then
+        --- This code gets the locale data for a single locale
+        --- The data objects contain all the locales in a single strings
+        --- Duplicated for performance reasons
+        local result = {}
+        for i = 1, #data do
+          local match = data[i]:match(localeToPattern[l10n.currentLocale])
+          result[i] = match ~= "" and match or nil
+        end
+        questCache[id] = result
+        return result
+      end
+      return nil
+    end
+
+    l10n.questName = function(id)
+      local questData = questCache[id] and questCache[id] or l10n.quest(id)
+      return questData and questData[1] or nil
+    end
+    -- TODO: This data is available but not used
+    l10n.questDescription = function(id)
+      local questData = questCache[id] and questCache[id] or l10n.quest(id)
+      return questData and questData[2] or nil
+    end
+    l10n.questObjectivesText = function(id)
+      local questData = questCache[id] and questCache[id] or l10n.quest(id)
+      return questData and questData[3] or nil
+    end
+  end
+end
+
+do
+  -- ? This function is used to export all the functions to the Public and Private namespaces
+  -- ? It gets called at the end of this file
+  local function exportFunctions()
+    local public = LibQuestieDB.PublicLibQuestieDB
+    public.ChangeLocale = function(locale)
+      if localeToPattern[locale] then
+        l10n.currentLocale = locale
+        SetGetters()
+      else
+        error(f("Invalid locale: %s", locale))
+      end
+    end
+
+    SetGetters()
+  end
+
+  exportFunctions()
+end
