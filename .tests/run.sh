ba44db43904771d82755a90f3682a7205cefa30a
Logon <Logon.q@gmail.com>
2024-04-15 14:04:53 +0200

    Initial commit for CI

--- .tests/run.sh
+++ .tests/run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+# This file is used by docker or local to run all tests
+# output is saved to file and error will show in the CLI
 
 cd /QuestieDB
+mkdir -p ./.tests/_data
 
 # Each expansion has its own dump process for each type
 for name in Era Tbc Wotlk
@@ -9,3 +12,5 @@ do
   echo "Log output saved to ./.tests/_data/$name-output.log"
   lua ./.tests/runTests.lua $name > ./.tests/_data/$name-output.log &
 done
+
+wait
