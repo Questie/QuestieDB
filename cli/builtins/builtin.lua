2601eaa8b1fbb002874090a5d33ec8a3029cc113
Logon <Logon.q@gmail.com>
2024-04-18 16:34:06 +0200

    feat(builtin.lua): enhance CLI with WoW API stubs for improved simulation
    - Add documentation links for each WoW API function stubbed.
    - Modify `UnitName` function to return "Reebookie" instead of "QuestieNPC" for better context in CLI.
    - Introduce LuaDoc comments for newly added functions to aid in development and debugging.
    - Refactor coroutine.yield assignment for clarity and maintainability.

--- cli/builtins/builtin.lua
+++ cli/builtins/builtin.lua
@@ -1,36 +1,60 @@
-coroutine.yield = _EmptyDummyFunction -- no need to yield in the cli (TODO: maybe find a less hacky fix)
+-- no need to yield in the cli (TODO: maybe find a less hacky fix)
+coroutine.yield = _EmptyDummyFunction
+
 mod = function(a, b)
   return a % b
 end
+
 hooksecurefunc = _EmptyDummyFunction
+
+---[Documentation](https://warcraft.wiki.gg/wiki/API_GetAddOnInfo)
 GetAddOnInfo = function()
   -- return "QuestieDB", "QuestieDB", "desc", true, "INSECURE", false
   return CLI_addonName or "QuestieDB", CLI_addonName or "QuestieDB", "desc", true, "INSECURE", false
 end
+
+---[Documentation](https://warcraft.wiki.gg/wiki/API_GetAddOnMetadata)
 GetAddOnMetadata = function()
   return "1"
 end
+
+---[Documentation](https://warcraft.wiki.gg/wiki/API_GetTime)
 GetTime = function()
+  ---@diagnostic disable-next-line: param-type-mismatch
   return os.time(os.date("!*t")) - 1616930000 -- convert unix time to wow time (actually accurate)
 end
+
+---[Documentation](https://warcraft.wiki.gg/wiki/API_IsAddOnLoaded)
 IsAddOnLoaded = function() return false, true end
+
+---[Documentation](https://warcraft.wiki.gg/wiki/API_UnitFactionGroup)
 UnitFactionGroup = function()
   return arg[1] or "Horde"
 end
+
+---[Documentation](https://warcraft.wiki.gg/wiki/API_UnitClass)
 UnitClass = function()
   return "Druid", "DRUID", 11
 end
+
+---[Documentation](https://warcraft.wiki.gg/wiki/API_UnitLevel)
 UnitLevel = function()
   return 60
 end
+
+---[Documentation](https://warcraft.wiki.gg/wiki/API_GetLocale)
 GetLocale = function()
   return "enUS"
 end
+
+---[Documentation](https://warcraft.wiki.gg/wiki/API_GetQuestGreenRange)
 GetQuestGreenRange = function()
   return 10
 end
+
+---[Documentation](https://warcraft.wiki.gg/wiki/API_UnitName)
 UnitName = function()
-  return "QuestieNPC"
+  return "Reebookie"
 end
 
 ---@diagnostic disable-next-line: lowercase-global
@@ -49,10 +73,12 @@ function Round(value)
 end
 
 local startTime = os.clock()
+---[Documentation](https://warcraft.wiki.gg/wiki/API_debugprofilestart)
 function debugprofilestart()
   startTime = os.clock()
 end
 
+---[Documentation](https://warcraft.wiki.gg/wiki/API_debugprofilestop)
 function debugprofilestop()
   return (os.clock() - startTime) * 1000
 end
